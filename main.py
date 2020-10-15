52f13680caed7a7e4f58d01350de4b241b78f7d2
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-10-15 00:26:45 -0300

    feat(graphing): added 2x5 subplots

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import sys
 from utils import visualize_path, get_graph_from_file
-from heuristic import euclidian_distance, manhattan_distance
+from heuristics import euclidian_distance, manhattan_distance
+
+
+def get_h_function(name):
+    if name == 'manhattan':
+        return manhattan_distance
+    elif name == 'euclidian':
+        return euclidian_distance
 
 
 def main():
     g = get_graph_from_file(sys.argv[1])
-    path_to_goal, visited = g.a_star(manhattan_distance)
-    visualize_path(g, path_to_goal, visited)
+
+    h_name = 'manhattan'
+
+    paths = []
+
+    goal_nodes, visited_nodes = g.depth_fs()
+    paths.append([goal_nodes, visited_nodes])
+
+    goal_nodes, visited_nodes = g.breadth_fs()
+    paths.append([goal_nodes, visited_nodes])
+
+    goal_nodes, visited_nodes = g.best_fs(get_h_function(h_name))
+    paths.append([goal_nodes, visited_nodes])
+
+    goal_nodes, visited_nodes = g.a_star(get_h_function(h_name))
+    paths.append([goal_nodes, visited_nodes])
+
+    goal_nodes, visited_nodes = g.a_star(get_h_function(h_name))
+    paths.append([goal_nodes, visited_nodes])
+
+    visualize_path(g, paths)
 
 
 if __name__ == '__main__':
