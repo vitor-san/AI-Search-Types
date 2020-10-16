8f3d709bc6dbb87bd82de7fc7ff38be22bca09ee
Victor Giovannoni <vernalhav@usp.br>
2020-10-16 12:09:55 -0300

    feat(main/metrics): added test case metric visualization and menu

--- main.py
+++ main.py
@@ -7,21 +7,13 @@ from metrics import get_metrics, plot_metrics
 TEST_FILES = [ os.path.join('test_cases', test) for test in os.listdir('test_cases') if test[-3:] == '.in' ]
 ALGORITHMS = ['dfs', 'bfs', 'best-fs', 'a-star', 'hill-climbing']
 
-def visualize_metrics(algorithm, heuristic):
-    metrics = []
-    for test_file in TEST_FILES:
-        g = get_graph_from_file(test_file)
-        metrics.append(get_metrics(g, algorithm, heuristic))
-    
-    plot_metrics(metrics, algorithm)
+def visualize_metrics(test_case, heuristic):
+    g = get_graph_from_file(test_case)
+    metrics = get_metrics(g, heuristic)    
+    plot_metrics(metrics, os.path.split(test_case)[1], heuristic)
 
 
 def main():
-    print('Em qual modo deseja executar o programa?')
-    print('0: Visualizar as métricas de um algoritmo para todos os casos de teste')
-    print('1: Visualizar caminhos dos algoritmos para um caso de teste')
-    option = int(input())
-
     print('Qual heurística deseja usar para as buscas informadas?')
     print('0: Distância Euclidiana')
     print('1: Distância Manhattan')
@@ -29,24 +21,13 @@ def main():
 
     heuristic = ['euclidian', 'manhattan'][heuristic_opt]
 
-    if option == 0:
-        print('Qual algoritmo?')
-        print('0: DFS')
-        print('1: BFS')
-        print('2: Best-first search')
-        print('3: A*')
-        print('4: Hill climbing')
+    print('Qual caso de teste?')
+    for i, test in enumerate(TEST_FILES):
+        print(f'{i+1}: {os.path.split(test)[1]}')
+    file = int(input())
 
-        algorithm_opt = int(input())
-        algorithm = ALGORITHMS[algorithm_opt]
-        visualize_metrics(algorithm, heuristic)
-    
-    elif option == 1:
-        print('Insira o nome do arquivo de teste:', end=' ')
-        filename = input()
-        get_graph_from_file(filename)
-        # TODO
+    visualize_metrics(TEST_FILES[file-1], heuristic)
 
 
 if __name__ == '__main__':
-    visualize_metrics('hill-climbing', 'manhattan')
+    main()
--- metrics.py
+++ metrics.py
@@ -13,10 +13,10 @@ def get_time(f, *args, **kwargs):
     return b - a, ret
 
 
-def plot_metrics(metrics, algorithm=None):
+def plot_metrics(metrics, test_case, heuristic):
 
     Ns = []
-    test_cases = [ f'{i+1}.in' for i in range(len(metrics)) ]
+    algorithm_names = [ metric['algorithm'] for metric in metrics ]
     times = []
     path_len = []
     found_goal = []
@@ -34,54 +34,67 @@ def plot_metrics(metrics, algorithm=None):
     fig, ((_,_), (len_ax, eff_ax), (speed_ax, bt_ax)) = plt.subplots(3, 2)   # Creating 3x2 grid
     time_ax = plt.subplot(3, 1, 1)  # Selecting first row as a single plot (execution time)
 
-    if algorithm is not None:
-        fig.suptitle(f'Métricas coletadas para o algoritmo {algorithm}')
+    fig.suptitle(f'Comparação de métricas dos algoritmos para o caso de teste {test_case} com heurística {heuristic}')
 
     # Configuring execution time data
-    time_ax.set_title(f'Tempo médio de {metrics[0]["n_repetitions"]} execuções por caso de teste')
-    time_ax.set_xlabel('Caso de teste')
+    time_ax.set_title(f'Tempo médio de {metrics[0]["n_repetitions"]} execuções por algoritmo')
     time_ax.set_ylabel('Média de tempo (s)')
-    time_ax.set_xticks(range(len(test_cases)))
-    time_ax.set_xticklabels(test_cases)
-    time_ax.bar(range(len(test_cases)), times, color=found_goal)
+    time_ax.set_xticks(range(len(algorithm_names)))
+    time_ax.set_xticklabels(algorithm_names)
+    time_ax.set_ylim(**(get_ylims(times, found_goal)))
+    time_ax.bar(range(len(algorithm_names)), times, color=found_goal)
 
     # Configuring path length data
-    len_ax.set_title('Tamanho do caminho encontrado por caso de teste')
-    len_ax.set_xlabel('Caso de teste')
+    len_ax.set_title('Tamanho do caminho encontrado por algoritmo')
     len_ax.set_ylabel('Tamanho do caminho')
-    len_ax.set_xticks(range(len(test_cases)))
-    len_ax.set_xticklabels(test_cases)
-    len_ax.bar(range(len(test_cases)), path_len, color=found_goal)
+    len_ax.set_xticks(range(len(algorithm_names)))
+    len_ax.set_xticklabels(algorithm_names)
+    len_ax.set_ylim(**(get_ylims(path_len, found_goal)))
+    len_ax.bar(range(len(algorithm_names)), path_len, color=found_goal)
 
     # Configuring efficiency data
-    eff_ax.set_title('Eficiência por caso de teste')
-    eff_ax.set_xlabel('Caso de teste')
+    eff_ax.set_title('Eficiência por algoritmo')
     eff_ax.set_ylabel('Eficiência')
-    eff_ax.set_xticks(range(len(test_cases)))
-    eff_ax.set_xticklabels(test_cases)
-    eff_ax.bar(range(len(test_cases)), efficiency, color=found_goal)
+    eff_ax.set_xticks(range(len(algorithm_names)))
+    eff_ax.set_xticklabels(algorithm_names)
+    eff_ax.set_ylim(**(get_ylims(efficiency, found_goal)))
+    eff_ax.bar(range(len(algorithm_names)), efficiency, color=found_goal)
 
     # Configuring speed data
-    speed_ax.set_title('Média de nós visitados por segundo por caso de teste')
-    speed_ax.set_xlabel('Caso de teste')
+    speed_ax.set_title('Média de nós visitados por segundo por algoritmo')
     speed_ax.set_ylabel('Nós por segundo')
-    speed_ax.set_xticks(range(len(test_cases)))
-    speed_ax.set_xticklabels(test_cases)
-    speed_ax.bar(range(len(test_cases)), speed, color=found_goal)
+    speed_ax.set_xticks(range(len(algorithm_names)))
+    speed_ax.set_xticklabels(algorithm_names)
+    speed_ax.set_ylim(**(get_ylims(speed, found_goal)))
+    speed_ax.bar(range(len(algorithm_names)), speed, color=found_goal)
 
     # Configuring backtracking data (TODO)
-    bt_ax.set_title('Número de backtracks por caso de teste')
-    bt_ax.set_xlabel('Caso de teste')
+    bt_ax.set_title('Número de backtracks por algoritmo')
     bt_ax.set_ylabel('Backtracks')
-    bt_ax.set_xticks(range(len(test_cases)))
-    bt_ax.set_xticklabels(test_cases)
-    # eff_ax.bar(range(len(test_cases)), speed, color=found_goal)
+    bt_ax.set_xticks(range(len(algorithm_names)))
+    bt_ax.set_xticklabels(algorithm_names)
+    # bt_ax.set_ylim(**(get_ylims(backtracks, found_goal)))
+    # eff_ax.bar(range(len(algorithm_names)), backtracks, color=found_goal)
 
     plt.tight_layout()
     plt.show()
 
 
-def get_metrics(g, algorithm_name, heuristic_name, n_repetitions=N_REPETITIONS):
+# Ignore data from algorithm if path wasn't found
+def get_ylims(data, found):    
+    max_y, min_y = -10000, 10000
+    for num, was_found in zip(data, found):
+        if was_found == 'g':
+            max_y = max(max_y, num)
+            min_y = min(min_y, num)
+
+    return {
+        'bottom': min_y*0.95,
+        'top': max_y*1.05
+    }
+
+
+def get_metrics(g, heuristic_name, n_repetitions=N_REPETITIONS):
 
     heuristic, path, visited, time = None, None, None, None
 
@@ -91,44 +104,29 @@ def get_metrics(g, algorithm_name, heuristic_name, n_repetitions=N_REPETITIONS):
     elif heuristic_name == 'manhattan':
         heuristic = manhattan_distance
 
-    if algorithm_name == 'bfs':
-        time, (path, visited) = get_time(g.breadth_fs)
+    algorithms = [g.breadth_fs, g.depth_fs, g.best_fs, g.a_star, g.hill_climbing]
+    algorithm_names = ['BFS', 'DFS', 'BeFS', 'A*', 'HC']
+    args = [[], [], [heuristic], [heuristic], [heuristic]]
+
+    metrics = []
+
+    for arg, algorithm, algorithm_name in zip(args, algorithms, algorithm_names):
+        time, (path, visited) = get_time(algorithm, *arg)
         for i in range(n_repetitions - 1):
-            time += get_time(g.breadth_fs)[0]
+            time += get_time(algorithm, *arg)[0]
         time /= n_repetitions
 
-    elif algorithm_name == 'dfs':
-        time, (path, visited) = get_time(g.depth_fs)
-        for i in range(n_repetitions - 1):
-            time += get_time(g.depth_fs)[0]
-        time /= n_repetitions
+        metrics.append({
+            'algorithm': algorithm_name,
+            'n_repetitions': n_repetitions,
+            'n_vertices': len(g.nodes),
+            'n_edges': len(g.edges),
+            'avg_time': time,
+            'backtracks': None,  # Número de backtracks parece ter que ser instrumentado na implementação
+            'path_len': len(path),
+            'found_goal': path[-1] == g.end,
+            'efficiency': len(path)/len(visited),
+            'speed': len(g.nodes)/(time + 1e-12) # Adding 10^-12 to prevent division by zero on fast executions
+        })
 
-    elif algorithm_name == 'best-first':
-        time, (path, visited) = get_time(g.best_fs, heuristic)
-        for i in range(n_repetitions - 1):
-            time += get_time(g.best_fs, heuristic)[0]
-        time /= n_repetitions
-
-    elif algorithm_name == 'a-star':
-        time, (path, visited) = get_time(g.a_star, heuristic)
-        for i in range(n_repetitions - 1):
-            time += get_time(g.a_star, heuristic)[0]
-        time /= n_repetitions
-
-    elif algorithm_name == 'hill-climbing':
-        time, (path, visited) = get_time(g.hill_climbing, heuristic)
-        for i in range(n_repetitions - 1):
-            time += get_time(g.hill_climbing, heuristic)[0]
-        time /= n_repetitions
-
-    return {
-        'n_repetitions': n_repetitions,
-        'n_vertices': len(g.nodes),
-        'n_edges': len(g.edges),
-        'avg_time': time,
-        'backtracks': None,  # Número de backtracks parece ter que ser instrumentado na implementação
-        'path_len': len(path),
-        'found_goal': path[-1] == g.end,
-        'efficiency': len(path)/len(visited),
-        'speed': len(g.nodes)/(time + 1e-12) # Adding 10^-12 to prevent division by zero on fast executions
-    }
+    return metrics
