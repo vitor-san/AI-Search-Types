a258be0b98c78c50ecac7478b782b9550eeebe20
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-10-18 16:50:31 -0300

    feat(main): added ENTER to input possibilities

--- main.py
+++ main.py
@@ -8,27 +8,41 @@ TEST_FILES = sorted([os.path.join('test_cases', test)
                      for test in os.listdir('test_cases') if test[-3:] == '.in'])
 
 
-def visualize_metrics(test_case, heuristic):
+def visualize_metrics(test_case, heuristic, mode):
     g = get_graph_from_file(test_case)
     metrics = get_metrics(g, heuristic)
-    plot_metrics(metrics, os.path.split(test_case)[1], heuristic)
+    plot_metrics(metrics, os.path.split(test_case)[1], heuristic, mode)
 
 
 def main():
-    print('Qual heurística deseja usar para as buscas informadas?')
+    print('Qual heurística deseja usar para as buscas informadas? (padrão = 1)\nPressione ENTER para escolher o padrão.\n')
     print('0: Distância Euclidiana')
     print('1: Distância Manhattan')
     print('2: Média entre euclidiana e manhattan')
-    heuristic_opt = int(input())
+
+    inp = input()
+    inp = 1 if inp == '' else inp
+    heuristic_opt = int(inp)
 
     heuristic = ['euclidian', 'manhattan', 'average'][heuristic_opt]
 
-    print('Qual caso de teste?')
+    print('\nQual caso de teste? (padrão = 4)\nPressione ENTER para escolher o padrão.\n')
     for i, test in enumerate(TEST_FILES):
         print(f'{i+1}: {os.path.split(test)[1]}')
-    file = int(input())
 
-    visualize_metrics(TEST_FILES[file-1], heuristic)
+    inp = input()
+    inp = 4 if inp == '' else inp
+    file = int(inp)
+
+    print('\nVocê quer ver as métricas de distância até o objetivo ou velocidade e número de backtracks? (padrão = 1)\nPressione ENTER para escolher o padrão.\n')
+    print('0: Distância até o objetivo')
+    print('1: Velocidade e número de backtracks')
+
+    inp = input()
+    inp = 1 if inp == '' else inp
+    mode = int(inp)
+
+    visualize_metrics(TEST_FILES[file-1], heuristic, mode)
 
 
 if __name__ == '__main__':
--- metrics.py
+++ metrics.py
@@ -14,7 +14,7 @@ def get_time(f, *args, **kwargs):
     return b - a, ret
 
 
-def plot_metrics(metrics, test_case, heuristic):
+def plot_metrics(metrics, test_case, heuristic, mode):
 
     Ns = []
     algorithm_names = [metric['algorithm'] for metric in metrics]
@@ -25,10 +25,11 @@ def plot_metrics(metrics, test_case, heuristic):
     backtracks = []
     speed = []
     images = []
+    distances = []
 
     for metric in metrics:
         Ns.append((metric['n_vertices'], metric['n_edges']))
-        times.append(metric['avg_time'])
+        times.append(1000*metric['avg_time'])
         path_len.append(metric['path_len'])
         backtracks.append(metric['backtracks'])
         # Setting red/green color if found
@@ -36,61 +37,97 @@ def plot_metrics(metrics, test_case, heuristic):
         efficiency.append(metric['efficiency'])
         speed.append(metric['speed'])
         images.append(metric['image'])
+        distances.append(metric['dist_to_goal'])
 
-    fig, ((_, _), (_, _), (len_ax, eff_ax), (speed_ax, bt_ax)
-          ) = plt.subplots(4, 2)   # Creating 4x2 grid
-    # Selecting second row of a 4x1 as a single plot (execution time)
-    time_ax = plt.subplot(4, 1, 2)
+    fig, len_ax, eff_ax, speed_ax, bt_ax, time_ax = None, None, None, None, None, None
 
-    # Displaying map images
-    for i, (image, algorithm_name) in enumerate(zip(images, algorithm_names)):
-        # Selecting the i-th subplot on the first row of a 4x5 grid
-        image_ax = plt.subplot(4, 5, i+1)
-        image_ax.set_title(algorithm_name)
-        image_ax.set_yticks([])
-        image_ax.set_xticks([])
-        image_ax.imshow(image, interpolation='nearest')
+    if mode == 0:
+        fig, axs = plt.subplots(2, 5)   # Creating 2x5 grid
 
-    fig.suptitle(
-        f'Comparação de métricas dos algoritmos para o caso de teste {test_case} com heurística {heuristic}')
+        # Displaying map images
+        for i, (image, algorithm_name) in enumerate(zip(images, algorithm_names)):
+            # Selecting the i-th subplot on the first row of a 4x5 grid
+            image_ax = plt.subplot(2, 5, i + 1)
+            image_ax.set_title(algorithm_name)
+            image_ax.set_yticks([])
+            image_ax.set_xticks([])
+            image_ax.imshow(image, interpolation='nearest')
 
-    # Configuring execution time data
-    time_ax.set_title(
-        f'Tempo médio de {metrics[0]["n_repetitions"]} execuções por algoritmo (em s)')
-    time_ax.set_xticks(range(len(algorithm_names)))
-    time_ax.set_xticklabels(algorithm_names)
-    time_ax.set_ylim(**(get_ylims(times, found_goal)))
-    time_ax.bar(range(len(algorithm_names)), times, color=found_goal)
+        max_dist = max([max(dist) for dist in distances])
 
-    # Configuring path length data
-    len_ax.set_title('Tamanho do caminho encontrado por algoritmo')
-    len_ax.set_xticks(range(len(algorithm_names)))
-    len_ax.set_xticklabels(algorithm_names)
-    len_ax.set_ylim(**(get_ylims(path_len, found_goal)))
-    len_ax.bar(range(len(algorithm_names)), path_len, color=found_goal)
+        # Displaying distance to goal
+        for i, dist in enumerate(distances):
+            dist_ax = plt.subplot(2, 5, 5 + i + 1)
+            dist_ax.plot(dist)
+            dist_ax.set_xlabel('Iteração')
+            dist_ax.set_ylim(bottom=0, top=max_dist)
+            if i == 0:
+                dist_ax.set_ylabel('Heurística até o objetivo')
 
-    # Configuring efficiency data
-    eff_ax.set_title('Eficiência por algoritmo')
-    eff_ax.set_xticks(range(len(algorithm_names)))
-    eff_ax.set_xticklabels(algorithm_names)
-    eff_ax.bar(range(len(algorithm_names)), efficiency, color=found_goal)
+        fig.suptitle(
+            f'Comparação de desempenho dos algoritmos para o caso de teste {test_case} com heurística {heuristic}')
 
-    # Configuring speed data
-    speed_ax.set_title('"Velocidade" por algoritmo (nós/segundo)')
-    speed_ax.set_xticks(range(len(algorithm_names)))
-    speed_ax.set_xticklabels(algorithm_names)
-    speed_ax.set_ylim(**(get_ylims(speed, found_goal)))
-    speed_ax.bar(range(len(algorithm_names)), speed, color=found_goal)
+        plt.subplots_adjust(left=0.055, right=0.98, top=0.96, hspace=0)
+        plt.show()
+        return
 
-    # Configuring backtracking data (TODO)
-    bt_ax.set_title('Número de backtracks por algoritmo')
-    bt_ax.set_xticks(range(len(algorithm_names)))
-    bt_ax.set_xticklabels(algorithm_names)
-    bt_ax.set_ylim(**(get_ylims(backtracks, found_goal)))
-    bt_ax.bar(range(len(algorithm_names)), backtracks, color=found_goal)
+    if mode == 1:
+        fig, ((_, _), (_, _), (len_ax, eff_ax), (speed_ax, bt_ax)
+              ) = plt.subplots(4, 2)   # Creating 4x2 grid
+        # Selecting second row of a 4x1 as a single plot (execution time)
+        time_ax = plt.subplot(4, 1, 2)
 
-    plt.subplots_adjust(bottom=0.06, hspace=0.4)
-    plt.show()
+        fig.suptitle(
+            f'Comparação de métricas dos algoritmos para o caso de teste {test_case} com heurística {heuristic}')
+
+        # Configuring speed data
+        speed_ax.set_title('"Velocidade" do algoritmo (nós/segundo)')
+        speed_ax.set_xticks(range(len(algorithm_names)))
+        speed_ax.set_xticklabels(algorithm_names)
+        speed_ax.set_ylim(**(get_ylims(speed, found_goal)))
+        speed_ax.bar(range(len(algorithm_names)), speed, color=found_goal)
+
+        # Configuring backtracking data
+        bt_ax.set_title('Número de backtracks do algoritmo')
+        bt_ax.set_xticks(range(len(algorithm_names)))
+        bt_ax.set_xticklabels(algorithm_names)
+        bt_ax.set_ylim(**(get_ylims(backtracks, found_goal)))
+        bt_ax.bar(range(len(algorithm_names)), backtracks, color=found_goal)
+
+        # Displaying map images
+        for i, (image, algorithm_name) in enumerate(zip(images, algorithm_names)):
+            # Selecting the i-th subplot on the first row of a 4x5 grid
+            image_ax = plt.subplot(4, 5, i + 1)
+            image_ax.set_title(algorithm_name)
+            image_ax.set_yticks([])
+            image_ax.set_xticks([])
+            image_ax.imshow(image, interpolation='nearest')
+
+        # Configuring execution time data
+        time_ax.set_title(
+            f'Tempo médio de {metrics[0]["n_repetitions"]} execuções do algoritmo (em milissegundos)')
+        time_ax.set_xticks(range(len(algorithm_names)))
+        time_ax.set_xticklabels(algorithm_names)
+        time_ax.set_ylim(**(get_ylims(times, found_goal)))
+        time_ax.bar(range(len(algorithm_names)), times, color=found_goal)
+
+        # Configuring path length data
+        len_ax.set_title('Tamanho do caminho encontrado pelo algoritmo')
+        len_ax.set_xticks(range(len(algorithm_names)))
+        len_ax.set_xticklabels(algorithm_names)
+        len_ax.set_ylim(**(get_ylims(path_len, found_goal)))
+        len_ax.bar(range(len(algorithm_names)), path_len, color=found_goal)
+
+        # Configuring efficiency data
+        eff_ax.set_title('Eficiência do algoritmo')
+        eff_ax.set_xticks(range(len(algorithm_names)))
+        eff_ax.set_xticklabels(algorithm_names)
+        eff_ax.bar(range(len(algorithm_names)), efficiency, color=found_goal)
+
+        plt.subplots_adjust(left=0.06, bottom=0.06,
+                            hspace=0.638, right=0.967, wspace=0.31)
+        plt.show()
+        return
 
 
 # Ignore data from algorithm in y limits if path wasn't found
@@ -134,19 +171,7 @@ def get_metrics(g, heuristic_name, n_repetitions=N_REPETITIONS):
             time += get_time(algorithm, *arg)[0]
         time /= n_repetitions
 
-        image = [[(0, 0, 0)] * g.cols for i in range(g.rows)]
-
-        for coord in g.graph.nodes:
-            image[coord[0]][coord[1]] = (255, 255, 255)
-
-        for coord in visited:
-            image[coord[0]][coord[1]] = (150, 150, 150)
-
-        for coord in path:
-            image[coord[0]][coord[1]] = (255, 204, 0)
-
-        image[g.start[0]][g.start[1]] = (51, 204, 51)
-        image[g.end[0]][g.end[1]] = (204, 0, 0)
+        dist_to_goal = [heuristic(coord, g.end) for coord in visited]
 
         metrics.append({
             'algorithm': algorithm_name,
@@ -158,9 +183,28 @@ def get_metrics(g, heuristic_name, n_repetitions=N_REPETITIONS):
             'path_len': len(path),
             'found_goal': path[-1] == g.end,
             'efficiency': len(path)/len(visited),
+            'dist_to_goal': dist_to_goal,
             # Adding 10^-12 to prevent division by zero on fast executions
             'speed': len(g.nodes)/(time + 1e-12),
-            'image': image
+            'image': get_image(g, visited, path)
         })
 
     return metrics
+
+
+def get_image(g, visited, path):
+    image = [[(0, 0, 0)] * g.cols for i in range(g.rows)]
+
+    for coord in g.graph.nodes:
+        image[coord[0]][coord[1]] = (255, 255, 255)
+
+    for coord in visited:
+        image[coord[0]][coord[1]] = (150, 150, 150)
+
+    for coord in path:
+        image[coord[0]][coord[1]] = (255, 204, 0)
+
+    image[g.start[0]][g.start[1]] = (51, 204, 51)
+    image[g.end[0]][g.end[1]] = (204, 0, 0)
+
+    return image
